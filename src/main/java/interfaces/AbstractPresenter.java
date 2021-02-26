6f3485913066e8d4ab3e35daf12e22e2de8a8c2e
ulyanovskk <mr1english4@gmail.com>
2021-02-26 15:07:11 +0400

    Deleted: everything

--- src/main/java/interfaces/AbstractPresenter.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public abstract class AbstractPresenter implements Presenter{
-    protected PresenterStatus presenterStatus = new PresenterStatusImpl();
-    protected abstract void createUI();
-    protected abstract void start();
-}
