b52f6a983eaf2a9c9c4ef4c787695385867f4c5c
ulyanovskk <mr1english4@gmail.com>
2021-03-07 17:51:06 +0300

    Added: HelpTest

--- src/test/java/commands/HelpTest.java
+++ src/test/java/commands/HelpTest.java
@@ -1,4 +1,22 @@
 package commands;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import logic.CMDManager;
+import logic.Editor;
+import org.junit.Test;
+
 public class HelpTest {
+    @Test
+    public void testRawHelp() {
+        CMDManager cmdManager = new CMDManager();
+        Editor editor = new Editor();
+        String result = cmdManager.execute(editor,"help");
+        FabricForCommands fabric = new FabricForCommands();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Command command: fabric.getAllCommandsArrayList())
+            stringBuilder.append(command.getDescription()).append("\n");
+        assertEquals(stringBuilder.toString().trim(), result);
+    }
 }
